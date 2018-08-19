fede24ad6be28337e8e0401381fa6302556f311f
Piorun <jakubm55555@gmail.com>
2018-08-20 01:46:04 +0200

    Edited lesson 4

--- CwiczeniaLearnCPP1/Lesson4.cpp
+++ CwiczeniaLearnCPP1/Lesson4.cpp
@@ -4,13 +4,6 @@
 void lesson4();
 int add(int x, int y);
 
-int add(int x, int y) //x and y are created here
-{
-	//x and y is vissible in this function only
-	return x + y;
-} // x and y go out of scope and are destroyed here
-
-
 void lesson4() //1.4d
 {
 	int a = 4; //a is created and initialized here
@@ -19,3 +12,9 @@ void lesson4() //1.4d
 	//calls function add, a = x, b = y;
 	std::cout << add(a, b) << std::endl;
 }	//a and b go out of scope and are destroyed here
+
+int add(int x, int y) //x and y are created here
+{
+	//x and y is vissible in this function only
+	return x + y;
+} // x and y go out of scope and are destroyed here
